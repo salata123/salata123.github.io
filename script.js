dc63ee58dc518240cd1699119f1dc981ea1247f6
Michal <salatman13@gmail.com>
2023-09-16 20:52:12 +0200

    25.1.1 test commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello';
+  const apiRoot = 'https://tasks--michaluwu.repl.co/v1/tasks';
+  const trelloApiRoot = 'https://tasks--michaluwu.repl.co/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
